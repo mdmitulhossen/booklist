9403cc461db278f434eed7eba852a0872e0b23dc
Mitul <md.mitul.hossen99@gmail.com>
2022-04-17 21:56:30 +0600

    frontpage-Done

---
+++ new.js
@@ -0,0 +1,10 @@
+// console.log(document.domain);
+// console.log(document.title);
+// console.log(document.domain);
+// // document.title=123;
+// console.log(document.doctype);
+
+var title=document.getElementById('title');
+console.log(title);
+      title.innerHTML = '<h6>hello</h6>';
+      title.style.border ='solid 2px red'
