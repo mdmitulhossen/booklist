605e34e90c7664cef8d6f49a8fcb3726deb8e8e0
Mitul <md.mitul.hossen99@gmail.com>
2022-04-18 12:45:39 +0600

    add remove alert-massage Done

--- book.js
+++ book.js
@@ -1,5 +1,35 @@
 var submit = document.getElementById('submit');
+const tableBody=document.querySelector('tbody');
 
+//deleteitem
+
+function deleteitem(el){
+      if(el.classList.contains('delete')){
+           el.parentElement.parentElement.remove();
+           showMassage('Book Removed!!','success');
+      }
+}
+
+//showMassage
+
+ showMassage= (massage,className)=>{
+      
+       this.massage=massage;
+       this.className=className;
+       const div = document.createElement('div');
+       div.className=`alert alert-${className}`;
+       div.appendChild(document.createTextNode(massage));
+
+       const container = document.querySelector('.container');
+       const form = document.querySelector('form');
+
+       container.appendChild(div);
+
+       setTimeout(()=>document.querySelector('.alert').remove(),3000);
+       
+ }
+
+//book add
 submit.addEventListener('click',submitbtn);
 function submitbtn(e){
       e.preventDefault();
@@ -8,27 +38,33 @@ function submitbtn(e){
       var author=document.getElementById('author');
       var isbn=document.getElementById('isbn');
 
-      console.log(title.value);
-      console.log(author.value);
-      console.log(isbn.value);
 
       //select tbody
+      if(title.value == ''|| author.value == ''|| isbn.value == ''){
+            showMassage('Please fill in the blank box!!','danger');
+      }
+      else{
+            
+            //craete element
 
-      var tableBody=document.querySelector('tbody');
-      console.log(tableBody);
+            var trow = document.createElement('tr');
 
-      //craete element
+            tableBody.appendChild(trow);
+            
+            trow.innerHTML = `
+                  <td>${title.value}</td>
+                  <td>${author.value}</td>
+                  <td>${isbn.value}</td>
+                  <td><a href="#" class="btn btn bg-danger text-white btn-sm delete">X</a></td>
 
-      var trow = document.createElement('tr');
+            `;
 
-      tableBody.appendChild(trow);
-      
-      trow.innerHTML = `
-           <td>${title.value}</td>
-           <td>${author.value}</td>
-           <td>${isbn.value}</td>
-           <td><a href="#" class="btn btn bg-danger text-white btn-sm delete">X</a></td>
-
-        `;
+            showMassage('Book Add!!','success');
+      }
 
 }
+
+//deleteitems
+ tableBody.addEventListener('click',(e)=>{
+    deleteitem(e.target);
+ })
